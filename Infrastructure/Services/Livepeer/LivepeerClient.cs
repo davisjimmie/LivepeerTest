08f31e954df6356a7f2f0144f581f87402c09bb0
Jimmie davis <jda2186@wgu.edu>
2024-12-24 03:02:12 -0800

    Add project files.

---
+++ Infrastructure/Services/Livepeer/LivepeerClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace LivepeerTest.Infrastructure.Services.Livepeer
+{
+    public class LivepeerClient
+    {
+        private readonly HttpClient _client;
+        private readonly string _apiKey;
+
+        public LivepeerClient(HttpClient client)
+        {
+            _apiKey = "48ca896f-048b-4697-bd5b-78554d9a38f1";
+            _client = client;
+
+            _client.BaseAddress = new Uri("https://livepeer.studio");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+        }
+
+        public HttpClient Client => _client;
+    }
+}
